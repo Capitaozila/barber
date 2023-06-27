19beee36ee8937e55f2565bb2b2c2e84fbe79bb1
Capitaozila <estudanteluis3030@gmail.com>
2023-06-27 14:01:52 -0300

    aplicativo completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BarberShopApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BarberShopApp extends StatelessWidget {
+  const BarberShopApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Barbearia',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: const BarberShopPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BarberShopPage extends StatefulWidget {
+  const BarberShopPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  createState() => _BarberShopPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BarberShopPageState extends State<BarberShopPage> {
+  static const int maxCadeirasEspera = 1;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int numClientesEspera = 0;
+  bool barbeiroDormindo = true;
+  bool cortandoCabelo = false;
+  int clienteAtual = 0;
+
+  final logger = Logger();
+
+  void adicionarCliente() {
+    if (barbeiroDormindo) {
+      setState(() {
+        barbeiroDormindo = false;
+        clienteAtual++;
+      });
+    } else if (numClientesEspera < maxCadeirasEspera) {
+      setState(() {
+        numClientesEspera++;
+        clienteAtual++;
+      });
+    } else {
+      logger.d('Cliente foi embora porque todas as cadeiras estavam ocupadas.');
+    }
+  }
+
+  void cortarCabeloBarbeiro() {
+    if (clienteAtual > 0 && !cortandoCabelo) {
+      setState(() {
+        cortandoCabelo = true;
+      });
+      Future.delayed(const Duration(seconds: 2), () {
+        setState(() {
+          numClientesEspera = numClientesEspera > 0 ? numClientesEspera - 1 : 0;
+          clienteAtual--;
+          cortandoCabelo = false;
+          barbeiroDormindo = clienteAtual == 0;
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Número de clientes esperando: $numClientesEspera',
+              style: const TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
             ),
+            const SizedBox(height: 20),
+            barbeiroDormindo
+                ? const Text(
+                    'O barbeiro está dormindo.',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  )
+                : Text(
+                    'O barbeiro está cortando o cabelo do cliente $clienteAtual.',
+                    style: const TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: adicionarCliente,
+              child: const Text('Adicionar Cliente'),
+            ),
+            const SizedBox(height: 20),
+            if (cortandoCabelo) ...[
+              const Text(
+                'Cortando o cabelo...',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(),
+            ],
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: cortarCabeloBarbeiro,
+        child: const Icon(Icons.cut),
+      ),
     );
   }
 }
